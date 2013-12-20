94f722977837998199fa98faaff68f274b060429
Joe Kutner <jpkutner@gmail.com>
2013-12-20 19:20:01 +0000

    Updating with latest episdoes

--- update.sh
+++ update.sh
@@ -3,8 +3,11 @@ source /home/tmux/.rvm/environments/ruby-1.9.3-p448
 cd /home/tmux/cbsfeed
 
 git pull origin gh-pages
+echo "Running the update script with bundler..."
 bundle exec ruby update.rb
+echo "Updating git repo..."
 git add _data/*.yml
 git commit -m "Updating with latest episdoes"
 git push origin gh-pages
+echo "All done!"
 
