42ef85245dbcc4b85f8fa76687c5fee3f0bc8744
Joe Kutner <jpkutner@gmail.com>
2014-05-13 20:48:41 -0500

    fixed date issue

--- update.rb
+++ update.rb
@@ -38,10 +38,18 @@ end
 class CbsEveningNewsEpisode < CbsEpisode
   def title
     unless @title
-      match_result = link.match(/(\d+-\d+)-/)
-      if match_result
+      if match_result = link.match(/(\d{1,2}-\d{1,2})-/)
         month_and_day = match_result[1].split('-')
         @title = "#{format('%02d', month_and_day[0])}-#{format('%02d', month_and_day[1])}"
+      elsif match_result = link.match(/(\d{2,4})-/)
+        dd = match_result[1]
+        if dd.size == 2
+          @title = "#{dd[0]}-#{dd[1]}"
+        elsif dd.size == 4
+          @title = "#{dd[0..1]}-#{dd[2..3]}"
+        else
+          @title = "#{dd[0]}-#{dd[1..2]}"
+        end
       else
         @title = '00-00'
       end
@@ -51,6 +59,7 @@ class CbsEveningNewsEpisode < CbsEpisode
 
   def date_digits
     @date_digits ||= time.strftime("%m%d")
+    
   end
 
   def time(raw_date=nil)
@@ -231,5 +240,5 @@ def update_evening_news
 end
 
 #update_tmz
-update_60min
+#update_60min
 update_evening_news
